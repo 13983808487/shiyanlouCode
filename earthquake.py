bd237f957c4cc7f1c22a21f972e259fd6d32b66c
13983808487 <851187378@qq.com>
2019-09-21 01:04:37 +0800

    week2-2nd-challenge

--- earthquake.py
+++ earthquake.py
@@ -8,5 +8,19 @@ def clean():
     df_clean = df_clean.dropna().drop_duplicates()
     return df_clean
 
+def mag_region():
+    df = clean()
+    df.mag = pd.cut(df.mag, bins=[0, 2, 5, 7, 9, 10], right=False,
+            labels=['micro', 'light', 'strong', 'major', 'great'])
+    df_group = df.groupby(by=['mag', 'region']).count()
+    df_reindex = df_group.reset_index().dropna()
+    df_sort = df_reindex.sort_values(
+            by='time', ascending=False).drop_duplicates('mag')
+    df_final = df_sort.set_index('mag')[['region', 'time']].rename(
+            columns={'time':'times'})
+    df_final.times = df_final.times.astype('int')
+
+    return df_final
+
 if __name__ == '__main__':
-    print(clean())
+    print(mag_region())
